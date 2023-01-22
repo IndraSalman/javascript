e086e390472fb00c9d4813030114a3c5181df47d
Indrasalman <indrasalman17@gmail.com>
2023-01-23 01:44:37 +0700

    mengenal Var, Let dan Const

---
+++ index.js
@@ -0,0 +1,15 @@
+// Nilai bisa di rubah
+var message = "Hello Coding"
+// Hanya berlaku di satu tempat
+let msg1 = "Hallo"
+// Nilai tidak bisa di rubah
+const msg2 = "Coding"
+
+if(true){
+    var message = 1
+    let msg1 = 2
+    console.log("testing", msg1)
+}
+
+console.log(message)
+console.log(msg1)
