e56117d03c8b35c79bccde25448bcc8c4989334a
Indrasalman <indrasalman17@gmail.com>
2023-01-23 02:02:09 +0700

    Membuat variabel tanpa perintah var

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-// Nilai bisa di rubah
-var message = "Hello Coding"
-// Hanya berlaku di satu tempat
-let msg1 = "Hallo"
-// Nilai tidak bisa di rubah
-const msg2 = "Coding"
+let msg1 = "Hallo";
+msg3 = "Berhasil";
+const msg2 = "Coding";
 
 if(true){
-    var message = 1
-    let msg1 = 2
-    console.log("testing", msg1)
+    let msg1 = "Yes";
+    var message = "ini message";
+    msg3 = "Selamat";
 }
-
-console.log(message)
-console.log(msg1)
+console.log(message);
